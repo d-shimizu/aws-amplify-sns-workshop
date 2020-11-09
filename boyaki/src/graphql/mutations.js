4de284060ffec47a188d71374353ac604d80e1f0
daisukeshimizu <shimizu.daisuke.sd@gmail.com>
2020-11-09 22:05:45 +0900

    リソースを再作成した

--- boyaki/src/graphql/mutations.js
+++ boyaki/src/graphql/mutations.js
@@ -1,22 +1,6 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createPost = /* GraphQL */ `
-  mutation CreatePost(
-    $input: CreatePostInput!
-    $condition: ModelPostConditionInput
-  ) {
-    createPost(input: $input, condition: $condition) {
-      type
-      id
-      content
-      owner
-      timestamp
-      createdAt
-      updatedAt
-    }
-  }
-`;
 export const updatePost = /* GraphQL */ `
   mutation UpdatePost(
     $input: UpdatePostInput!
@@ -86,6 +70,91 @@ export const deleteFollowRelationship = /* GraphQL */ `
       followeeId
       followerId
       timestamp
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateTimeline = /* GraphQL */ `
+  mutation UpdateTimeline(
+    $input: UpdateTimelineInput!
+    $condition: ModelTimelineConditionInput
+  ) {
+    updateTimeline(input: $input, condition: $condition) {
+      userId
+      timestamp
+      postId
+      post {
+        type
+        id
+        content
+        owner
+        timestamp
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteTimeline = /* GraphQL */ `
+  mutation DeleteTimeline(
+    $input: DeleteTimelineInput!
+    $condition: ModelTimelineConditionInput
+  ) {
+    deleteTimeline(input: $input, condition: $condition) {
+      userId
+      timestamp
+      postId
+      post {
+        type
+        id
+        content
+        owner
+        timestamp
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createPost = /* GraphQL */ `
+  mutation CreatePost(
+    $input: CreatePostInput!
+    $condition: ModelPostConditionInput
+  ) {
+    createPost(input: $input, condition: $condition) {
+      type
+      id
+      content
+      owner
+      timestamp
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createTimeline = /* GraphQL */ `
+  mutation CreateTimeline(
+    $input: CreateTimelineInput!
+    $condition: ModelTimelineConditionInput
+  ) {
+    createTimeline(input: $input, condition: $condition) {
+      userId
+      timestamp
+      postId
+      post {
+        type
+        id
+        content
+        owner
+        timestamp
+        createdAt
+        updatedAt
+      }
       createdAt
       updatedAt
     }
